1d6ee7b937093ff923cdeee891058d73e3071fe3
D R N D A <82357097+SCARY-114@users.noreply.github.com>
2021-04-15 02:31:31 +0300

    Update fcx.py

--- fcx.py
+++ fcx.py
@@ -1,6 +1,6 @@
 import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass, time
 os.system('rm -rf .txt')
-for n in range(8000):
+for n in range(20202):
     nmbr = random.randint(1000000, 9999999)
     sys.stdout = open('.txt', 'a')
     print nmbr
